e8012344f7c7fd3290eb100b60cf3470b118bd54
Fischer Robert <robert.fischer@psi.ch>
2022-08-17 13:56:11 +0200

    inlcuded all? filters?

--- filter_functions.py
+++ filter_functions.py
@@ -3,6 +3,14 @@
 Created on Mon Aug 15 16:35:22 2022
 
 @author: fische_r
+
+
+class to create feature stack on 4D tomo data
+transient dimension, e.g. time, should be 4th dimension of input data
+
+
+TODO: add GPU support (CUDA, cupy, cucim)
+
 """
 
 # necessary packages for feature stack
@@ -44,6 +52,13 @@ default_feature_dict = {'Gaussian': True,
                'extra_time_ranks': True,
               }
 
+def ball_4d(sig):
+    bnd = np.zeros((sig*2+1,sig*2+1,sig*2+1,sig*2+1), dtype = bool)
+    bnd[sig,sig,sig,sig] = True
+    ecd = ndimage.distance_transform_edt(~bnd)
+    bnd = (ecd<sig+0.01).astype(int)
+    return bnd
+
 class image_filter:
     def __init__(self,
                  data_path = None,
@@ -52,7 +67,9 @@ class image_filter:
                  feature_dict = default_feature_dict,
                  mod_feat_dict = None,
                  chunksize = '20 MiB',
-                 outchunks = '300 MiB'
+                 outchunks = '300 MiB',
+                 ranks = ['maximum', 'minimum', 'mean', 'median'],
+                 sigma_t = 40
                  ):
         if mod_feat_dict is not None:
             for key in mod_feat_dict:
@@ -68,9 +85,13 @@ class image_filter:
         
         # not sure if this is clever, does dask understand that this data is reused?
         self.Gaussian_4D_dict = {}
+        self.Gaussian_space_dict = {}
+        self.Gaussian_time_dict = {}
         self.Gradient_dict = {}
         self.calculated_features = []
         self.feature_names = []
+        self.considered_ranks = ranks
+        self.sigma_t = sigma_t
         
         
     def open_raw_data(self):
@@ -92,7 +113,29 @@ class image_filter:
         G = self.data.map_overlap(filters.gaussian, depth=deptharray, boundary='constant', sigma = sigma)
         self.feature_names.append('Gaussian_4D_Blur_'+f'{sigma:.1f}')
         self.calculated_features.append(G)
-        self.Gaussian_dict[sigma] = G
+        self.Gaussian_4D_dict[sigma] = G
+        
+    def Gaussian_Blur_space(self, sigma):      
+        deptharray = np.ones(self.data.ndim)+4*sigma
+        deptharray[-1] = 0
+        deptharray = tuple(np.min([deptharray, self.data.shape], axis=0))
+        sigmas = np.ones(deptharray.shape)*sigma
+        sigmas[-1] = 0
+        G = self.data.map_overlap(filters.gaussian, depth=deptharray, boundary='constant', sigma = sigmas)
+        self.feature_names.append('Gaussian_space_'+f'{sigma:.1f}')
+        self.calculated_features.append(G)
+        self.Gaussian_space_dict[sigma] = G
+
+    def Gaussian_Blur_time(self, sigma):      
+        deptharray = np.ones(self.data.ndim)+4*sigma
+        deptharray[:-1] = 0
+        deptharray = tuple(np.min([deptharray, self.data.shape], axis=0))
+        sigmas = np.ones(deptharray.shape)*sigma
+        sigmas[:-1] = 0
+        G = self.data.map_overlap(filters.gaussian, depth=deptharray, boundary='constant', sigma = sigmas)
+        self.feature_names.append('Gaussian_time_'+f'{sigma:.1f}')
+        self.calculated_features.append(G)
+        self.Gaussian_time_dict[sigma] = G
         
     def Gaussian_4D_stack(self):
         flag = True
@@ -105,20 +148,65 @@ class image_filter:
             else:
                 self.Gaussian_Blur_4D(sigma)
                 
-    def diff_Gaussian_4D(self):
-        for comb in self.Gaussian_4D_dict.keys():
-            DG = self.Gaussian_4D_dict[comb[1]] - self.Gaussian_4D_dict[comb[0]]
-            name = ''.join(['diff_of_gauss_4D_','_',comb[1],'_',comb[0]])
+    def Gaussian_space_stack(self):
+        flag = True
+        for sigma in self.sigmas:
+            if np.abs(sigma-0)<0.1:
+                if flag:
+                    flag = False
+                    self.Gaussian_space_dict['original'] = self.data
+            else:
+                self.Gaussian_Blur_space(sigma)
+                
+    def Gaussian_time_stack(self):
+        flag = True
+        for sigma in self.sigmas:
+            if np.abs(sigma-0)<0.1:
+                if flag:
+                    flag = False
+                    self.Gaussian_time_dict['original'] = self.data
+            else:
+                self.Gaussian_Blur_time(sigma)
+                
+            
+    def diff_Gaussian(self, mode):
+        if mode == '4D':
+            lookup_dict = self.Gaussian_4D_dict
+        elif mode == 'space':
+            lookup_dict = self.Gaussian_space_dict
+        elif mode == 'time':
+            lookup_dict = self.Gaussian_time_dict
+            
+        for comb in lookup_dict.keys():
+            DG = lookup_dict[comb[1]] - lookup_dict[comb[0]]
+            name = ''.join(['diff_of_gauss_',mode,'_',comb[1],'_',comb[0]])
             self.calculated_features.append(DG)
             self.feature_names.append(name)
-    
+            
+    def diff_to_first_and_last(self):
+        options = self.Gaussian_space_dict.keys()
+        for opt in options:
+            DA = self.Gaussian_space_dict[opt]
+            first = DA[...,0]
+            last = DA[...,-1]
+            DF = DA.copy()
+            DL = DA.copy()
+            for i in range(DA.shape[-1]):
+                curr = DA[...,i]
+                DF[...,i] = curr-first
+                DL[...,i] = curr-last
+            name1 = 'diff_to_first_'+opt
+            name2 = 'diff_to_last_'+opt
+            self.calculated_features.append(DF)
+            self.calculated_features.append(DL)
+            self.feature_names.append(name1)
+            self.feature_names.append(name2)
+            
+            
     def Gradients(self):
         for key in self.Gaussian_dict:
             G = self.Gaussian_dict[key]
             gradients = dask.array.gradient(G)
-            # axes = G.ndim
-            # for ax0 in range(axes):
-                # gradients[ax0].name = 'Gradient_sigma_'+key+'_'+str(ax0)
             self.Gradient_dict[key] = gradients
             
     def Hessian(self):
@@ -135,23 +223,86 @@ class image_filter:
             self.feature_names = self.feature_names + gradnames + hessnames
             self.calculated_features = self.calculated_features+gradients+H_elems
     
+    def rank_filter(self, option, sigma):
+        # note: rank filters not yet available in CUCIM
+        da = self.data
+        if not np.abs(sigma-0)<1:
+            if option == 'minimum':
+                fun = filters.rank.minimum
+            elif option == 'maximum':
+                fun = filters.rank.maximum
+            elif option == 'median':
+                fun = filters.rank.median
+            elif option == 'mean':
+                fun = filters.rank.mean     
+
+            fp = ball_4d(sigma)            
+            deptharray = np.ones(da.ndim)+sigma
+            deptharray = tuple(np.min([deptharray, da.shape], axis=0))
+                     
+            R = da.map_overlap(fun, depth=deptharray, footprint=fp)
+            name = ''.join([option,'_',f'{sigma:.1f}'])
+            self.calculated_features.append(R)
+            self.feature_names.append(name)
+    
+    def dynamic_rank_filter(self, option, sigma):
+        # TODO: add custom dynamic model, eg. sigmoid
+        da = self.data
+        if option == 'minimum':
+            fun = filters.rank.minimum
+        elif option == 'maximum':
+            fun = filters.rank.maximum
+        elif option == 'median':
+            fun = filters.rank.median
+        elif option == 'mean':
+            fun = filters.rank.mean  
+            
+        fp_3D = ball(sigma)
+        fp_4D = np.zeros(list(fp_3D.shape)+[2*self.sigma_t], dtype=int)
+        fp_4D[fp_3D>0,:] = 1
+        deptharray = np.ones(da.ndim)+sigma
+        deptharray[-1] = self.sigma_t
+        deptharray = tuple(np.min([deptharray, da.shape], axis=0))
+        
+        R = da.map_overlap(fun, depth=deptharray, footprint=fp_4D)
+        name = ''.join([option,'_dynamic_',f'{sigma:.1f}'])
+        self.calculated_features.append(R)
+        self.feature_names.append(name)
+                
+            
+    def rank_filter_stack(self):
+        for option in self.considered_ranks:
+            for sigma in self.sigmas:
+                self.rank_filter(option, sigma)
+                self.dynamic_rank_filter(option, sigma)
+                
+    
     # TODO: include feature selection either in compute (better) or save
     def prepare(self):
-        self.Gaussian_stack()
-        self.diff_Gaussian_4D()
+        self.Gaussian_4D_stack()
+        self.diff_Gaussian('4D')
         self.Gradients()
         self.Hessian()
+        self.Gaussian_time_stack()
+        self.diff_Gaussian('time')
+        self.Gaussian_space_stack()
+        self.diff_Gaussian('space')
+        self.rank_filter_stack()
+
         
     def compute(self):
         self.prepare()
         for feat in self.calculated_features:
+            # are multiplely used intermediate results persistent or recalculated, e.g. Gaussian Blur? what about spilling to disk?
             feat.compute()
         self.feature_stack = dask.array.stack(self.calculated_features, axis = 4)
         self.feature_stack.rechunk(self.outchunks)
         
-    def store_xarray_nc(self):
+    def store_xarray_nc(self, outpath = None):
+        if outpath is None:
+            outpath = self.outpath
         shp = self.feature_stack.shape
-        # TODO: take coordinates from tomodata dataset and get correct feature name
+        # TODO: take coordinates from tomodata dataset
         self.result = xr.Dataset({'feature_stack': (['x','y','z','time', 'feature'], self.feature_stack)},
                                  coords = {'x': np.arange(shp[0]),
                                            'y': np.arange(shp[1]),
@@ -159,7 +310,7 @@ class image_filter:
                                            'time': np.arange(shp[3]),
                                            'feature': self.feature_names}
                                  )
-        self.result.to_netcdf4(self.outpath)
+        self.result.to_netcdf4(outpath)
         
         
         
